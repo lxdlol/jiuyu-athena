8189decbd327e4d1fbad218607f9c53a20f9dccc
chenxiaoli <39185753@qq.com>
2020-03-20 19:11:47 +0800

    modeling

--- models/project.go
+++ models/project.go
@@ -13,12 +13,12 @@ type Project struct {
 	Comments  []Comment `json:"comments" bson:"comments"`
 	Follows   []User    `json:"follows" bson:"follows"`
 	Reports   []Report  `json:"reports" bson:"reports"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	CreatedAt time.Time `json:"created_at" bson:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" bson:"updated_at"`
 }
 type Report struct {
-	User      User
-	Title     string
+	User      User   `json:"user" bson:"user"`
+	Title     string `json:"title" bson:"tile"`
 	FilePath  string
 	CreatedAt time.Time
 	UpdatedAt time.Time
