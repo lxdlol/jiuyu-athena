fd98d1e6ec98be5740e1797f32e1abc70f6f93ef
chenxiaoli <39185753@qq.com>
2020-03-23 14:37:32 +0800

    config method

---
+++ config/config.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"sync"
+)
+
+type AthenaConfig struct {
+	DataSource DataSourceConfig `json:"dataSource"`
+
+	Kafka      KafkaConfig      `json:"kafka"`
+	PushServer PushServerConfig `json:"pushServer"`
+	RestServer RestServerConfig `json:"restServer"`
+	JwtSecret  string           `json:"jwtSecret"`
+}
+
+type DataSourceConfig struct {
+	DriverName        string `json:"driverName"`
+	Host              string `json:"host"`
+	Port              int64  `json:"port"`
+	Database          string `json:"database"`
+	User              string `json:"user"`
+	Password          string `json:"password"`
+	EnableAutoMigrate bool   `json:"enableAutoMigrate"`
+}
+
+type KafkaConfig struct {
+	Brokers []string `json:"brokers"`
+}
+
+type PushServerConfig struct {
+	Addr string `json:"addr"`
+	Path string `json:"path"`
+}
+
+type RestServerConfig struct {
+	Addr string `json:"addr"`
+}
+
+var config AthenaConfig
+var configOnce sync.Once
+
+func GetConfig() *GbeConfig {
+	configOnce.Do(func() {
+		bytes, err := ioutil.ReadFile("config.json")
+		if err != nil {
+			panic(err)
+		}
+
+		err = json.Unmarshal(bytes, &config)
+		if err != nil {
+			panic(err)
+		}
+	})
+	return &config
+}
