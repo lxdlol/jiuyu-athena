9036da65eee7cb2fd792ea42f3dce63afd69c9b2
chenxiaoli <39185753@qq.com>
2020-03-24 15:37:12 +0800

    ignore

--- utils/kafka.go
+++ utils/kafka.go
@@ -1,1 +1,45 @@
 package utils
+
+import "time"
+import "github.com/segmentio/kafka-go"
+
+type KafkaReader struct {
+	reader *kafka.Reader
+}
+
+type KafkaWriter struct {
+	writer *kafka.Writer
+}
+
+var kafkaReader *KafkaReader //单列模式
+var kafkaWriter *KafkaWriter
+
+func GetKafkaReader(topic string, brokers []string) *KafkaReader {
+	if kafkaReader == nil {
+		kafkaReader := &KafkaReader{}
+
+		kafkaReader.writer = kafka.NewReader(kafka.ReaderConfig{
+			Brokers:   brokers,
+			Topic:     topic,
+			Partition: 0,
+			MinBytes:  1,
+			MaxBytes:  10e6,
+		})
+	}
+	return kafkaReader
+}
+
+func GetKafkaWriter(topic string, brokers []string) *KafkaWriter {
+	if kafkaWriter == nil {
+		kafkaWriter := &KafkaWriter{}
+
+		kafkaWriter.writer = kafka.NewWriter(kafka.WriterConfig{
+			Brokers:      brokers,
+			Topic:        topic,
+			Balancer:     &kafka.LeastBytes{},
+			BatchTimeout: 5 * time.Millisecond,
+		})
+	}
+
+	return kafkaWriter
+}
