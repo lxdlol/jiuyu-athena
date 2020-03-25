0d6e14955630ebac28eb5e3fdde1253d7520c7ee
FTXMJ <15044827567@163.com>
2020-03-25 16:26:06 +0800

    错误返回 规定包上传 修复

--- common/errhandle.go
+++ common/errhandle.go
@@ -1,7 +1,7 @@
 package common
 
 type ResponseData struct {
-	Code int         `json:"code""`
+	Code int         `json:"code"`
 	Msg  string      `json:"msg"`
 	Data interface{} `json:"data,omitempty"`
 }
